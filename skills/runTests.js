48c49ced8077490d53caba5c082b654737eaa4b4
Annie Wang <annie97wang@gmail.com>
2018-07-25 15:27:57 -0700

    added new files

---
+++ skills/runTests.js
@@ -0,0 +1,45 @@
+var functions = require("./functions");
+
+module.exports = async function (bot,message,body) {
+    var reqBody;
+    var command;
+    if (body === null) command = null;
+    else command = body.command;
+    console.log("IN RUN TESTS: command is "+ body);
+    reqBody = {intent: "runTests", state: 0};
+    try {
+        var res = await functions.sendRequest(reqBody);
+        var body = JSON.parse(res.body);
+        bot.startConversation(message, async function (err, convo) {
+        switch(body.state) {
+          case "prompt":
+            convo.ask(body.content, async function (response, convo){
+                console.log("USER RESPONSE IS: " + JSON.stringify(response.text));
+                reqBody = {intent: "runTests", state: 1, command: response.text};
+                var result = await functions.sendRequest(reqBody);
+                convo.say(JSON.parse(result.body).content);
+                convo.next();
+            });
+         break;
+          
+          case "success":
+          case "fail":
+            convo.say(body.content);
+            convo.next();
+            break;
+            
+          case "error" :
+            convo.say(body.content);
+            break;
+            
+          default: 
+            console.error("in pull default case");
+            break;
+                         }
+        })
+    }
+    catch (err) {
+        console.error("In runTests, caught an error:" + err);
+        return;
+    }
+}
