dd4ba987db69fbb2133895fc2e4b6218d00ebc51
Annie Wang <annie97wang@gmail.com>
2018-07-30 16:15:01 -0700

    updated checkoutBranch.js

--- skills/checkoutBranch.js
+++ skills/checkoutBranch.js
@@ -9,34 +9,25 @@ module.exports = async function (bot,message, branchName) {
         var body = JSON.parse(res.body);
         bot.startConversation(message, async function (err, convo) {
         switch(body.state) {
-          case "prompt":
-            convo.ask(body.content, async function (response, convo){
-                console.log("USER RESPONSE IS: " + JSON.stringify(response.text));
-                reqBody = {intent: "runTests", state: 1, command: response.text};
-                var result = await functions.sendRequest(reqBody);
-                convo.say(JSON.parse(result.body).content);
-                convo.next();
-            });
-         break;
     
           case "success":
-          case "fail":
             convo.say(body.content);
             convo.next();
             break;
             
           case "error" :
             convo.say(body.content);
+            convo.next();
             break;
             
           default: 
-            console.error("in pull default case");
+            console.error("in checkoutBranch default case");
             break;
                          }
         })
     }
     catch (err) {
-        console.error("In runTests, caught an error:" + err);
+        console.error("In checkoutBranch, caught an error:" + err);
         return;
     }
 }
